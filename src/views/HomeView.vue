1bacc7e08b08845dadfd4132ba4b97e7cb697238
todd <linxin5661@gmail.com>
2023-04-20 17:26:49 +0800

    npm run build -> dist

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      voluptates, quod, quia, voluptate quae quidem voluptatem quos
+      voluptatum quibusdam quas quia voluptate quae quidem voluptatem quos
+      voluptatum quibusdam quas
+    </p>
   </main>
 </template>
---
+++ src/views/PointView.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <h1>Home</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quod, quia, voluptate quae quidem voluptatem quos
+            voluptatum quibusdam quas quia voluptate quae quidem voluptatem quos
+            voluptatum quibusdam quas
+        </p>
+    </main>
+</template>
