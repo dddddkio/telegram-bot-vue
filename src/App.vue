b5694c1e95d03f4e22d7e38c3e7f4f3c88edca97
todd <linxin5661@gmail.com>
2023-04-30 23:15:46 +0800

    webapp

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,60 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue'
-
-const checkedOptions = ref(['￥5 - 1点'])
-const options = ['￥5 - 1点', '￥10 - 2点', '￥15 - 3点', '￥20 - 4点']
-
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <!-- 
-    <div class="wrapper">
+  <div style="margin-top:5vh">
+    <div>Your current points balance is: <text class="text-bold text-black" style="font-size: large;">xxx</text></div>
+    <div style="margin-top:10px;margin-bottom: 15px;">Just pay and get points now:</div>
 
-      
-      
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/point">Point</RouterLink> 
-        
+    <el-table 
+      ref="table" 
+      :data="tableData" 
+      style="width: 100%" 
+      @row-click="handleRowClick" 
+      highlight-current-row>
+      <el-table-column prop="Combo" label="Combo" />
+      <el-table-column prop="Points" label="Points" />
+      <el-table-column prop="UnitPrice" label="UnitPrice" />
+    </el-table>
 
-    </div>
-    -->
-  </header>
-
-  <!--
-  <RouterView />
-  -->
-  <div><text class="text-bold text-black" style="font-size: large;">Hi, xxx! </text>您的账户目前余额 <text
-      class="text-red">2</text> 点</div>
-  <div style="margin-top:10px">套餐：
-    <div style="margin-top:5px">
-      <el-checkbox-group v-model="checkedOptions" :min="0" :max="1">
-        <el-checkbox v-for="option in options" :key="option" :label="option">{{
-          option
-        }}</el-checkbox>
-      </el-checkbox-group>
-    </div>
+    <el-button type="success" style="margin-top:3vh;width:100%" @click="handleButtonClick">去支付</el-button>
   </div>
-
-  <el-button type="success" style="margin-top:10px;width:100%">去支付</el-button>
-
-  <div class="bg-grey light solid-xl shadow" style="padding:10px;border-radius: 2%;margin-top: 15px;">
-    提示1：如果支付完成，VIP没有到账，请退出重新登录一次即可。如果还未到账可以发工单联系客服。</div>
-  <div class="bg-grey light solid-xl shadow" style="padding:10px;border-radius: 2%;margin-top: 10px;">
-    提示2：月付用户没有手机等客户端权限，需要手机客户端的请选择季付及以上套餐。</div>
-  <div class="bg-grey light solid-xl shadow" style="padding:10px;border-radius: 2%;margin-top: 10px;">
-    提示3：部分订单更改为HKD港币结算，最终换算为人民币付款时可能会因为汇率原因产生部分价格差异。</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          Combo: "$4.99",
+          Points: "50",
+          UnitPrice: "$0.1",
+        },
+        {
+          Combo: "$21.99",
+          Points: "300",
+          UnitPrice: "$0.073",
+        },
+        {
+          Combo: "$78.99",
+          Points: "1500",
+          UnitPrice: "$0.0526",
+        },
+        {
+          Combo: "$129.99",
+          Points: "3000",
+          UnitPrice: "$0.0433",
+        },
+      ],
+    };
+  },
+  methods: {
+    
+    handleRowClick(row, event) {
+      console.log('选中了第' + (this.tableData.indexOf(row) + 1) + '行');
+      console.log(row)
+    },
+  },
+};
+</script>
 
 
 <style scoped>
@@ -57,47 +63,7 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.row-selected {
+  background-color: #e6f7ff !important;
 }
 </style>
