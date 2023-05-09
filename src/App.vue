27ca3c45bfac7d042d5de540958db4c1a205622c
todd <linxin5661@gmail.com>
2023-05-10 01:07:24 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@ export default {
     },
     handleButtonClick(){
       alert("You have paid successfully!")
-      //Telegram.WebApp.senData(this.row)
+      Telegram.WebApp.senData(this.row)
       Telegram.WebApp.close()
     }
   },
